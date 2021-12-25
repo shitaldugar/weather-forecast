ce9e6280a98597169cb35edf0a0cc190c369aa8b
shitaldugar <shitalsonawat@gmail.com>
2021-12-25 21:39:52 +0530

    updated

---
+++ src/components/Title.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+ const Title=()=>{
+     return(
+         <>
+         <div>
+        <h1>Weather App</h1>
+        <h3>Find out the weather forecast around you<br/>
+             so you can prepare yourself</h3>
+            
+         </div>
+         </>
+     )
+
+ }
+ export default Title;
